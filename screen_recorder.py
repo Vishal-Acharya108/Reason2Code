fb5cf3f00b3f181bddbe8eb51d971ce6a8a59aa4
Vishal Acharya <152165077+Vishal-Acharya108@users.noreply.github.com>
2026-01-01 21:49:29 +0530

    python program for screen recording

---
+++ screen_recorder.py
@@ -0,0 +1,21 @@
+import numpy as np
+import cv2
+import pyautogui
+screen_width, screen_height = pyautogui.size()
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+output = cv2.VideoWriter(
+    r"E:\Reason 2 code\output.avi",
+    fourcc,
+    20.0,
+    (screen_width, screen_height)
+)
+while True:
+    img = pyautogui.screenshot()
+    frame = np.array(img)
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+    output.write(frame)
+    cv2.imshow("Screen Recorder", frame)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+output.release()
+cv2.destroyAllWindows()
